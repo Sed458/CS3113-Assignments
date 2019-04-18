52d4edb86c5e8184207fe9f1d0e6d6e4aad739f9
Sed458 <sed458@nyu.edu>
2019-04-17 22:11:11 -0400

    Platformer

---
+++ HW#4/NYUCodebase/MySprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="MySprites" tilewidth="20" tileheight="20" spacing="3" margin="2" tilecount="900" columns="30">
+ <image source="spritesheet_rgba.png" width="692" height="692"/>
+</tileset>
